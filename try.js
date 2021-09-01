0b13f494b366500b084bde9bed09001ba840399e
Nayeem-seu45 <2017000000034@seu.edu.bd>
2021-09-01 11:19:23 +0600

    myvery first commit

---
+++ try.js
@@ -0,0 +1,44 @@
+/* function MyName()
+{
+ var name = "Nayeem";
+ console.log(MyName); 
+}
+
+ */
+
+//2
+/* var name = "nayeem";
+function myfun(){
+
+}
+
+console.log(name); */
+
+//variabe hoisting
+
+name = "Nayeem";
+console.log(name);
+var name;
+
+//loop
+
+/* var i;
+for(i=1;i<=10;i++)
+{
+    console.log(i);
+} */
+
+//for of loop
+
+/* var myCountry = ['Dhaka', 'Barisal', 'Rangpur', 'Chandpur']
+
+for(let myCity of myCountry){
+    console.log(myCity);
+} */
+
+
+//oop
+
+var billGates = {shirt: true,shirtColor:"offWhite", smile:true,swatter:true}
+
+
