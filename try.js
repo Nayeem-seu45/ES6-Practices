f64f11b61c0fdefe6321bfa926f83076319513c9
Nayeem-seu45 <2017000000034@seu.edu.bd>
2021-09-01 17:28:29 +0600

    doneESseries

--- try.js
+++ try.js
@@ -16,9 +16,9 @@ console.log(name); */
 
 //variabe hoisting
 
-name = "Nayeem";
+/* name = "Nayeem";
 console.log(name);
-var name;
+var name; */
 
 //loop
 
@@ -52,12 +52,12 @@ var billGatesPro = {
  */
 
 //for in loop
-var billGates = {shirt: true,shirtColor:"offWhite", smile:true,swatter:true}
+/* var billGates = {shirt: true,shirtColor:"offWhite", smile:true,swatter:true}
 
 for (let props in billGates){
     //console.log(billGates[props]);
     console.log(props +"="+ billGates[props]);
-}
+} */
 
 //simple function
 
@@ -84,7 +84,7 @@ myNew(30,21); */
 
 //Rest parameter
 
-function something(...x)
+/* function something(...x)
 {
     console.log(x);
     console.log(x[4]);
@@ -99,22 +99,171 @@ function myFunction()
 }
 
 console.log(myFunction());
-
+ */
 //annoymous function
 
-var myfun = function(){
+/* var myfun = function(){
     return "hello"
 }
 
 var myfun = function(){
     return "hello monu"
 }
-console.log(myfun());
+console.log(myfun()); */
 
 //arrow function
 
-var myArrow =()=>{
+/* var myArrow =()=>{
     console.log("My first Arrow function");
 }
 
-myArrow();
+myArrow(); */
+
+//array
+
+/* var myArray = new Array ("a","b","c","d")
+
+for(let item of myArray)
+{
+    console.log(item);
+} */
+
+//multidimentonal array
+
+/* var bangladesh = ["Dhaka", "chandpur", "Rangpur"]
+var india = ["Mumbai","Channai", "Kolkhata"]
+
+var asia = [bangladesh,india]
+
+console.log(asia[0][1]); */
+
+//de-structure
+
+/* var countries = ["BAN","IND","XA","PAk"]
+
+var[, ,a,c] = countries
+
+console.log(a,c); */
+
+//map
+
+/* var myMap= new Map();
+myMap.set("key1", "Bangladesh");
+myMap.set("key2", "India");
+myMap.set("key3", "Maldiv");
+myMap.set("key4", "Hongkong");
+myMap.set("key5", "Nepal"); */
+//console.log(myMap.set());
+//console.log(myMap.values());
+
+//myMap.delete("key2");
+
+//myMap.get("key3")
+//myMap.clear();
+/* for(let item of myMap.values()){
+    console.log(item);
+} */
+
+/* if(myMap.has("key10")){
+    console.log("Yes");
+}
+else{
+    console.log("Nop");
+
+} */
+//set
+/* var mySet = new Set();
+
+mySet.add('Bangladesh');
+mySet.add('Nepal');
+mySet.add('Barisal');
+
+console.log(mySet); */
+
+//class
+
+/* class MyClass
+{
+    myFun()
+    {
+        console.log("Hello Sabbir Bhai");
+    }
+    myFun2()
+    {
+        console.log("Bhai kto number chole");
+    }
+    myFun3()
+    {
+        console.log("Bhai .....!!!!");
+    }
+    myFun4(name)
+    {
+        console.log(name);
+    }
+}
+
+var obj = new MyClass;
+obj.myFun2();
+obj.myFun4("valo Lage nah");
+ */
+//constructor
+
+/* class myClasss{
+    constructor(){
+        console.log("Hello I am Nayeem");
+    }
+}
+
+new myClasss(); */
+
+/* class myClass{
+    constructor(a,b){
+        //console.log(a+b);
+        this.firsthNum = a
+        this.secondNum = b
+    }
+
+    add(){
+        let result = this.firsthNum+ this.secondNum;
+        console.log(result);
+    }
+}
+
+var obj = new myClass(10,30);
+obj.add(); */
+
+//static keyword
+
+/* class myClass{
+    
+
+   static hello(){
+       console.log("hello vlo achoni");
+
+    }
+}
+
+myClass.hello(); */
+
+//Inheritence
+
+/* class parent{
+    hello(params)
+    {
+        console.log("hello sss");
+    }
+}
+
+class child extends parent{
+    hello(params)
+    {
+        console.log("hello ms. ami tmr theke oo powerfull");
+    }
+
+}
+var obj = new child();
+obj.hello(); */
+
+//Module
+
+
